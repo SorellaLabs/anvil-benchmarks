f5d0fd50e314cb03ce7ee77c32d196d6fa310e7e
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-06-09 02:58:54 +0200

    rate limit

--- benches/anvil-benchmarks.rs
+++ benches/anvil-benchmarks.rs
@@ -156,5 +156,5 @@ async fn spawn_http(local: bool) -> Result<SpawnResult, Box<dyn Error>> {
     spawn_with_config(config).await
 }
 
-criterion_group!(benches, benchmark_ipc, benchmark_http_local, benchmark_http, benchmark_reth);
+criterion_group!(benches, benchmark_ipc, benchmark_http_local, benchmark_reth);
 criterion_main!(benches);
