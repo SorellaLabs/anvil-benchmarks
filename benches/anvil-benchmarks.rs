5281152ae6c8546bc36bb79ee635a7bbceb53fbc
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-06-09 02:57:10 +0200

    rate limit

--- benches/anvil-benchmarks.rs
+++ benches/anvil-benchmarks.rs
@@ -149,7 +149,7 @@ async fn spawn_http(local: bool) -> Result<SpawnResult, Box<dyn Error>> {
         .with_steps_tracing(false)
         .with_tracing(false)
         .silent()
-        .fork_compute_units_per_second(Some(250))
+        .fork_compute_units_per_second(Some(100))
         .fork_request_retries(Some(12))
         .fork_request_timeout(Some(Duration::from_millis(50000)));
 
